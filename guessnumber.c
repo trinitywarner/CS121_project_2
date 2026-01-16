8585aace2b1e1bce472561bc801bad414f64a7b7
trinitywarner <trinity.warner@bsu.edu>
2026-01-16 21:16:06 +0000

    Self Attempt 1...

---
+++ guessnumber.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TRUE -1
+#define FALSE 0
+
+
+
+int main(){
+
+	char name [20];
+	printf("What is your name? ");
+	scanf("%s", name);
+	printf("Hi, %s! \n", name);
+
+	int keepGoing TRUE
+	char guess[10];
+	int turns = 7;
+	srand(time(NULL));
+
+	while (keepGoing){
+		turns++;
+		printf("Please guess the correct number: ");
+		scanf(%s", guess);
+
+		if (stcmp(guess, correct) == 0){
+			printf("Winner winner, chicken dinner!");
+			keepGoing = FALSE;
+		} else {
+			if (turns > 7){
+				printf("Not good.");
+				keepGoing = FALSE;
+			if (turns < 7)
+				printf("Not bad, but not good.")
+				keepGoing = FALSE;
+			}
+		} // end else
+	} //end while
+return 0;
+} //end main
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+char* = correct 21
+
+int main(){
+	char response[10];
+	strcpy(response, "guess");
+	int keepGoing = =1;
+	srand(time(NULL));
+
+
+
+	while(keepGoing){
+		if (strcmp(response, "21) == 0){
+			keepGoing = 0;
+		} // end if
+	} //end while
+	  print
+
